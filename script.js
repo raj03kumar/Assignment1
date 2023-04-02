2d092906fa8a83866d426825a741f0c0d5c3cc8a
Raj <singhraj1553@outlook.com>
2023-04-03 00:03:13 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,9 @@
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["frontend development", "competitive programming", "machine learning"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
